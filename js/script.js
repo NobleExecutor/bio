1b11df0319e0d2576ea00a57b5fd6a8917c6eb6b
NobleExecutor <brakes_juniors.0t@icloud.com>
2026-03-31 17:27:05 +0200

    Changed background, changed typewriter effect to display 2 messages

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 let textElement = document.getElementById("typewriter");
-const text = "Hello, World! ";
+const messages = [
+  "Hello, World! ",
+  "Bienvenue sur mon Portfolio! "
+];
+let currentMessageIndex = 0;
+let text = messages[currentMessageIndex];
+
 let textContent = '';
 let textIndex = 0;
 let isTextDeleting = false;
 let textCursorVisible = true;
 
 document.addEventListener("DOMContentLoaded", function() {
-    function typeWriterText() {
+  function typeWriterText() {
     if (!isTextDeleting && textIndex < text.length) {
-        textContent = text.slice(0, textIndex + 1);
-        textIndex++;
+      textContent = text.slice(0, textIndex + 1);
+      textIndex++;
     } else if (isTextDeleting && textIndex > 0) {
-        textContent = text.slice(0, textIndex - 1);
-        textIndex--;
+      textContent = text.slice(0, textIndex - 1);
+      textIndex--;
     } else if (textIndex === text.length) {
-        isTextDeleting = true;
-        setTimeout(typeWriterText, 5000);
-        return;
-    } else if (textIndex === 0) {
-        isTextDeleting = false;
+      isTextDeleting = true;
+      setTimeout(typeWriterText, 2000);
+      return;
+    } else if (textIndex === 0 && isTextDeleting) {
+      isTextDeleting = false;
+      currentMessageIndex = (currentMessageIndex + 1) % messages.length;
+      text = messages[currentMessageIndex];
+      setTimeout(typeWriterText, 500);
+      return;
     }
+
     textElement.textContent = textContent + (textCursorVisible ? '|' : ' ');
-    setTimeout(typeWriterText, isTextDeleting ? 150 : 300);
-}
+    setTimeout(typeWriterText, isTextDeleting ? 100 : 150);
+  }
 
-typeWriterText();
+  typeWriterText();
 
-setInterval(() => {
+  setInterval(() => {
     textCursorVisible = !textCursorVisible;
     textElement.textContent = textContent + (textCursorVisible ? '|' : ' ');
-}, 500);
-})
+  }, 500);
+});
