d76f882ba4ac43fc6aac6843fd2f6bbf71c81f6b
NobleExecutor <brakes_juniors.0t@icloud.com>
2026-04-09 18:07:32 +0200

    Added GSAP animations triggered on scroll

---
+++ js/anim.js
@@ -0,0 +1,61 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".about-wrapper", {
+    duration: 1,
+    x: -1500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top 70%"
+    },
+    ease: "power2.out"
+});
+
+const educationTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#education",
+        start: "top 70%"
+    }
+});
+
+educationTl.from(".education-container", {
+    duration: 0.6,
+    y: 1000,
+    opacity: 0,
+    scale: 0.2,
+    ease: "power2.out"
+});
+
+educationTl.from(".education-item", {
+    x: -60,
+    opacity: 0,
+    duration: 0.8,
+    stagger: 0.3,
+    ease: "power2.out"
+});
+
+const projectsTl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#projects",
+        start: "top 70%",
+    }
+});
+
+projectsTl.from(".projects-card", {
+    y: 100,
+    opacity: 0,
+    duration: .6,
+    stagger: .3
+});
+
+gsap.from(".contact-container", {
+    opacity: 0,
+    scale: 0,
+    duration: .4,
+    scrollTrigger: {
+        trigger: "#contact",
+        start: "center 70%",
+        markers: true
+    },
+    ease: "power2.out"
+});
